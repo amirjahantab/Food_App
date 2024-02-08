0ead5ce22d452f06e8fb4f3f5324a0d1fe10dd25
Amirreza Jahantab <jahantab83@gmail.com>
2024-02-08 18:50:56 +0330

    add class base view and update template

--- food/models.py
+++ food/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
 class Item(models.Model):
         
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.TextField()
     item_price = models.IntegerField()
@@ -11,3 +14,8 @@ class Item(models.Model):
 
     def __str__(self):
         return self.item_name
+    
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
+        
+    
--- food/views.py
+++ food/views.py
@@ -2,39 +2,66 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
+from django.views.generic import ListView, DeleteView
+from django.views.generic.edit import CreateView
 
 # Create your views here.   
-def index(request):
-    item_list = Item.objects.all()
-    context = {
-        'item_list': item_list
-    }
-    return render(request, 'food/index.html', context)
+# def index(request):
+#     item_list = Item.objects.all()
+#     context = {
+#         'item_list': item_list
+#     }
+#     return render(request, 'food/index.html', context)
+
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+    
 
 
 def item(request):
     return HttpResponse("Hello, world. You're at the polls item.")
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item': item
-    }
-    return render(request, 'food/detail.html', context)
 
-def create_item(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('food:index')
-    else:
-        form = ItemForm()
+# def detail(request, item_id):
+#     item = Item.objects.get(pk=item_id)
+#     context = {
+#         'item': item
+#     }
+#     return render(request, 'food/detail.html', context)
+
+
+class FoodDetail(DeleteView):
+    model = Item
+    template_name = 'food/detail.html'
+    context_object_name = 'item'
+
+
+# def create_item(request):
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('food:index')
+#     else:
+#         form = ItemForm()
         
-    context = {
-        'form': form
-    }
-    return render(request, 'food/item-form.html', context)
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'food/item-form.html', context)
+
+
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+    
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 
 def update_item(request, item_id):
